908484a11992f8baddafb09e89872186088eb185
victorashino <victor.ashino.main@gmail.com>
2025-03-20 01:20:34 -0300

    first commit

---
+++ src/main/java/dev/bicutoru/orderflow/OrderflowApplication.java
@@ -0,0 +1,13 @@
+package dev.bicutoru.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bicutoru/orderflow/OrderflowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bicutoru.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
