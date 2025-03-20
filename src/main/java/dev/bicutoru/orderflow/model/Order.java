e5bb4f158a49b5175376ae06300840eebf43b9cb
victorashino <victor.ashino.main@gmail.com>
2025-03-20 03:26:26 -0300

    Configuração do banco de dados e conexão com o Spring Boot

--- src/main/java/dev/bicutoru/orderflow/model/Order.java
+++ src/main/java/dev/bicutoru/orderflow/model/Order.java
@@ -14,17 +14,21 @@ import java.util.List;
 @AllArgsConstructor
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String date;
 
     @ManyToOne
+    @JoinTable(name = "customer_id")
+    private Customer customer;
+
+    @ManyToMany
     @JoinTable(
             name = "order_product",
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "product_id")
     )
-    private Customer customer;
-
     private List<Products> products;
 }
